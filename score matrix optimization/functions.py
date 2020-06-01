e98ad1b5efad9428d27735da9e5c325fb705583f
Mpaperlee <Mpaperlee@Hotmail.com>
2020-06-01 12:00:43 +0800

    del opt_1,2,3 renew function.py

--- score matrix optimization/functions.py
+++ score matrix optimization/functions.py
@@ -9,49 +9,14 @@ import os
 import re
 import string
 import math
-
 import numpy as np
 import pandas as pd
-
 
 def replace_all(repls, str):
     """Replace all the elements in the repls dictionary  of strings,
     For example, replace dict{key(current):value(target)} """
     return re.sub('|'.join(re.escape(key) for key in repls.keys()),
                   lambda k: repls[k.group(0)], str) 
-
-
-def symarray(a):
-    num = len(a)
-    for i in range(0,num):
-        for j in range(0,num):
-            if i >= j :
-                continue
-            a[j][i] = a[i][j]
-    return a
-
-
-def order_diag(num):   
-    """the order of downward spiral on the diagonal(within)"""
-    count = 0
-    length = int((num * num- num)/2 + num)
-    order = np.empty([length,2],dtype = int)
-    for i in range(0,2*num-1):
-        if i%2 == 0:
-            a = int(i/2)
-            b = int((i+1)/2)
-        else:
-            a = int((i-1)/2)
-            b = int((i+2)/2)
-        while a>=0 and b<=(num-1):
-            order[count][0] = a
-            order[count][1] = b
-            count = count + 1
-            a = a - 1
-            b = b + 1
-    #print(order)
-    return order
-
 
 def mkdir(dir_name):
     if os.path.exists(dir_name):
@@ -62,33 +27,6 @@ def mkdir(dir_name):
     print('A directory named '+dir_name+' has being created.')
     os.system(mkdir)
 
-
-def fake_generate(file_name,dir_clt,num):
-    '''Using permutation generate all the fake cell lineage tree'''
-    startTime = datetime.datetime.now()
-    result = pd.read_csv(file_name,delimiter='\t')['Class'].value_counts().to_frame()
-    list_index = result.index.tolist()[0:num]####
-    string_letter0 = string.ascii_uppercase[0:num]
-    list_letter0 = list(string_letter0)
-    mapping0 = dict(zip(list_letter0,list_index))
-    mkdir(dir_clt)
-    text = open(file_name).read()
-    count_ftree = 0
-    for i in itertools.permutations(string_letter0, num):
-        print(count_ftree)
-        string_letter1 = ' '.join(i)
-        transform_mapping = replace_all(mapping0,string_letter1)
-        list_letter_trans = transform_mapping.split()
-        mapping1 = dict(zip(list_index,list_letter_trans))
-        print(mapping1)
-        transform_result = replace_all(mapping1,text)
-        str_open = './'+dir_clt+'/'+dir_clt+'_%d.alm' % count_ftree
-        count_ftree +=1
-        f_in = open(str_open, "w")
-        f_in.write(transform_result)
-        f_in.close()
-    time = datetime.datetime.now() - startTime
-    print(time)
 
 
 def fake_select_generate(file_name,dir_clt,list_cell_type):
--- score matrix optimization/opt.py
+++ score matrix optimization/opt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+# Optimize to get a better score matrix(Fig.7 and supply Fig.4)
+# Before run this script, move the DELTA app(from ./app/) to the current directory. The final optmiazed matrix is in the '~/depth_3/round_17/df_matrix.csv'
 import datetime
 import itertools
 import linecache
@@ -16,61 +17,45 @@ import numpy as np
 import pandas as pd
 from numpy import genfromtxt
 
-from functions import *
+from functions import mkdir, fake_select_generate, DataFrame2vector, HSA_g, grep_score, list_mapping, np_vsplit, order_asym_new2
 
 startTime = datetime.datetime.now()
+
 #parameters
+# use order_asym_new2 num_split = 4
 file_name0 = "fun.alm"
 file_name1 = "pma.alm"
-num_split = 6
+num_split = 4
 value_range = 1
 scale = 2
 depth = 3
 fake_max = 1000
 prune = 10 * value_range
 repeat = 1
-witch_fake = False #"True" mean file_name0, "False" mean file_name1
 
 #initialize
 result0 = pd.read_csv(file_name0,delimiter='\t')['Class'].value_counts().to_frame()
 num0 = int(len(result0.index))
+clt_name0 = str(file_name0.split('.')[0])
 sort_cell_type0 = dict(zip(list(string.digits[0:num0]), result0.index))
 
 result1 = pd.read_csv(file_name1,delimiter='\t')['Class'].value_counts().to_frame()
 num1 = int(len(result1.index))
+clt_name1 = str(file_name1.split('.')[0])
 sort_cell_type1 = dict(zip(list(string.digits[0:num1]), result1.index))
 
-num_max = max(num0,num1)
-matrix_zero = np.zeros((num0,num1), dtype = int)
-
-if witch_fake == True:
-    clt_name = str(file_name0.split('.')[0])
-    sort_cell_type = dict(zip(list(string.digits[0:num0]),result0.index))
-    file_name = file_name0
-    file_name_solid = file_name1
-    num = num0
-    result = result0
-else:
-    clt_name = str(file_name1.split('.')[0])
-    sort_cell_type = dict(zip(list(string.digits[0:num1]),result1.index))
-    file_name = file_name1
-    file_name_solid = file_name0
-    num = num1
-    result = result1
+num_max = max(num0, num1)
+matrix_zero = np.zeros((num0, num1), dtype=int)
 
 #generate the main directory
-dir_0 = "./new_ v    result.vs_fake." + clt_name + "_split." +str(num_split) + "_depth." + str(depth) +"_fm." +str(fake_max)+"_r." + str(repeat)
+dir_0 = "./new_result_split." +str(num_split) + "_depth." + str(depth) + "_r." + str(repeat)
 mkdir(dir_0)
-dir_clt_0 = dir_0 + "/" + clt_name 
+dir_clt_0 = dir_0 + "/" + clt_name0
 mkdir(dir_clt_0)
+dir_clt_1 = dir_0 + "/" + clt_name1
+mkdir(dir_clt_1)
 
-#generate all the fake trees of whole cell types 
-dir_clt_full = dir_clt_0 + "/" + clt_name + "_full"
-
-list_cell_type_full = list_mapping(sort_cell_type, list(range(0,num)))
-fake_select_generate(file_name, dir_clt_full, list_cell_type_full)
-
-np_order = order_diag(num_max)
+np_order = order_asym_new2(num0, num1)
 np_order_splited = np_vsplit(np_order, num_split)
 turns = len(np_order_splited)
 
@@ -80,11 +65,12 @@ for L in range(depth + 1):
     mkdir(dir_1)
     #update the matrix by the current optimization
     if L > 0:
-        dir_matrix = dir_0 + "/depth_" +str(L - 1) + "/round_"+ str(turns - 1) + "/df_matrix.csv"
+        dir_matrix = dir_0 + "/depth_" + \
+            str(L - 1) + "/round_" + str(turns - 1) + "/df_matrix.csv"
         matrix_pd = pd.read_csv(dir_matrix, delimiter=" ")
-        matrix_zero = matrix_pd.iloc[:,1:].values
+        matrix_zero = matrix_pd.iloc[:, 1:].values
 
-    def order_split(num_split,num):
+    def order_split(num_split, num):
         rounds = 0
         while rounds < turns:
             print(rounds)
@@ -112,39 +98,43 @@ for L in range(depth + 1):
 
             order_i_list1 = list(set(lst1))
             list_cell_type1 = list_mapping(sort_cell_type1, order_i_list1)
-            if witch_fake == True:
-                order_i_list = order_i_list0
-                list_cell_type = list_cell_type0
-            else:
-                order_i_list = order_i_list1
-                list_cell_type = list_cell_type1
 
-            #generate all fake trees of associated cell types 
-            fake_count = reduce(lambda x,y:x*y, range(1,len(order_i_list) + 1))
+            #generate all fake trees of associated cell types
+            fake_count0 = reduce(lambda x, y: x*y, range(1, len(order_i_list0) + 1))
+            """if len(order_i_list0) == num0:
+                dir_clt0 = dir_clt_full0
+            else:"""
+            dir_clt0 = dir_clt_0 + "/" + clt_name0 + "_%d" % rounds
+            if L == 0:
+                mkdir(dir_clt0)
+                fake_select_generate(file_name0, dir_clt0, list_cell_type0)
 
-            if len(order_i_list) == num:
-                dir_clt = dir_clt_full
-            else:
-                dir_clt = dir_clt_0 + "/" + clt_name + "_%d" % rounds
-                if L == 0:
-                    mkdir(dir_clt)
-                    fake_select_generate(file_name, dir_clt, list_cell_type)
+            fake_count1 = reduce(lambda x, y: x*y, range(1, len(order_i_list1) + 1))
+            """if len(order_i_list1) == num1:
+                dir_clt1 = dir_clt_full1
+            else:"""
+            dir_clt1 = dir_clt_1 + "/" + clt_name1 + "_%d" % rounds
+            if L == 0:
+                mkdir(dir_clt1)
+                fake_select_generate(file_name1, dir_clt1, list_cell_type1)
 
             #generate all the different matrixs
             np_matrix = np.zeros(shape=(2**num_split, num_split))
             names = dict()
-            num_element = 0 #num_elementi(max)= len(order)
-            for x,y in order_i:
+            num_element = 0  # num_elementi(max)= len(order)
+            for x, y in order_i:
                 number = int(matrix_zero[x][y])
                 if L > 0:
                     if number > 0:
-                        names["element%s" % num_element] = range(number * scale - 1, number * scale + 1)
+                        names["element%s" % num_element] = range(
+                            number * scale - 1, number * scale + 1)
                     else:
-                        names["element%s" % num_element] = range(number * scale, number * scale + 2)
+                        names["element%s" % num_element] = range(
+                            number * scale, number * scale + 2)
                 else:
-                    names["element%s" % num_element] = [-1,1]
+                    names["element%s" % num_element] = [-1, 1]
                 num_element += 1
-            
+
             pools = [tuple(vales_range) for vales_range in names.values()]
             result = [[]]
             for pool in pools:
@@ -156,42 +146,45 @@ for L in range(depth + 1):
                 matrix_name = dir_matrix+"/matrix_%d.csv" % count_matrix
                 count_matrix = count_matrix + 1
                 z = 0
-                for x,y in order_i:
-                    matrix_zero[x][y] = p[z]# Somehow need to test symmetry
+                for x, y in order_i:
+                    matrix_zero[x][y] = p[z]  # Somehow need to test symmetry
                     z = z + 1
                 print(matrix_zero)
                 DataFrame2vector(result0, result1, matrix_zero, matrix_name)
-            x = "There is "+str(count_matrix)+ " matrixs are genetated"
+            x = "There is "+str(count_matrix) + " matrixs are genetated"
             print(x)
 
-            df_parameter = pd.DataFrame(columns=['target','time','remark'])
-            for matrix_NO in range(0,count_matrix):
-            #for matrix_NO in range(1,11): ## small test
+            df_parameter = pd.DataFrame(columns=['target', 'time', 'remark'])
+            for matrix_NO in range(0, count_matrix):
+                #for matrix_NO in range(1,11): ## small test
                 try:
-                    #Generate HSA scripts   
+                    #Generate HSA scripts
                     HSA_scripts = dir_script + "/run_HSA_%d.sh" % matrix_NO
                     f_script = open(HSA_scripts, "w+")
                     f_script.write("#!/bin/bash\n")
                     file_matrix = dir_matrix+"/"+"matrix_%d.csv" % matrix_NO
                     file_result_real = dir_result+"/result.almg"
-                    f_script.write(HSA_g(file_name, file_name_solid, file_matrix, file_result_real, prune)) 
+                    f_script.write(HSA_g(file_name0, file_name1, file_matrix, file_result_real, prune))
 
-                    if fake_count > fake_max:
-                        #sum_rand = 0
-                        for fake_NO in range(0,fake_max):
-                            matrix_rand = randint(0,fake_count-1)
-                            fake_clt = dir_clt+"/"+clt_name+"_%d.alm" % matrix_rand
+                    if fake_count0*fake_count1 > fake_max:
+                        for fake_NO in range(0, fake_max):
+                            matrix_rand0 = randint(0, fake_count0-1)
+                            matrix_rand1 = randint(0, fake_count1-1)
+                            fake_clt0 = dir_clt0+"/"+clt_name0+"_%d.alm" % matrix_rand0
+                            fake_clt1 = dir_clt1+"/"+clt_name1+"_%d.alm" % matrix_rand1
                             file_result = dir_result+"/result_%d.almg" % fake_NO
-                            f_script.write(HSA_g(fake_clt, file_name_solid, file_matrix, file_result, prune))
-                            #sum_rand = sum_rand + matrix_rand
-                        #random_seed = sum_rand/fake_max
+                            f_script.write(HSA_g(fake_clt0, fake_clt1, file_matrix, file_result, prune))
                         f_script.close()
 
                     else:
-                        for fake_NO in range(0,fake_count):
-                            fake_clt = dir_clt+"/"+clt_name+"_%d.alm" % fake_NO
-                            file_result = dir_result+"/result_%d.almg" % fake_NO
-                            f_script.write(HSA_g(fake_clt, file_name_solid, file_matrix, file_result, prune))
+                        fake_NO = 0
+                        for fake_NO_0 in range(0, fake_count0):
+                            for fake_NO_1 in range(0, fake_count1):
+                                fake_clt0 = dir_clt0+"/"+clt_name0+"_%d.alm" % fake_NO_0
+                                fake_clt1 = dir_clt1+"/"+clt_name1+"_%d.alm" % fake_NO_1
+                                file_result = dir_result+"/result_%d.almg" % fake_NO
+                                fake_NO += 1
+                                f_script.write(HSA_g(fake_clt0, fake_clt1, file_matrix, file_result, prune))
                         f_script.close()
                     print("HSA scipts are generated well")
 
@@ -200,22 +193,22 @@ for L in range(depth + 1):
                     print("HSA ran well done")
 
                     #alalysis and calculate the target
-                    np_result = dir_np_result +"/np_result_%d.txt" % matrix_NO
+                    np_result = dir_np_result + "/np_result_%d.txt" % matrix_NO
                     f_np_result = open(np_result, "w+")
-                    
+
                     Global_score = []
                     Parameter = []
                     score_real = grep_score(file_result_real)
                     print(score_real)
                     f_np_result.write(str(score_real))
 
-                    if fake_count > fake_max:
-                        for fake_NO in range(0,fake_max):
+                    if fake_count0*fake_count1 > fake_max:
+                        for fake_NO in range(0, fake_max):
                             file_result = dir_result+"/result_%d.almg" % fake_NO
                             Global_score.append(grep_score(file_result))
 
                     else:
-                        for fake_NO in range(0,fake_count):
+                        for fake_NO in range(0, fake_count0*fake_count1):
                             file_result = dir_result+"/result_%d.almg" % fake_NO
                             Global_score.append(grep_score(file_result))
 
@@ -240,17 +233,18 @@ for L in range(depth + 1):
                 except:
                     dir_error = dir_1 + "/error_log.txt"
                     f = open(dir_error, 'a')
-                    traceback.print_exc(file=f)   
-                    f.flush()   
-                    f.close()   
+                    traceback.print_exc(file=f)
+                    f.flush()
+                    f.close()
 
             df_matrix = pd.DataFrame(np_matrix)
-            df_result = pd.concat([df_parameter,df_matrix], axis=1)
+            df_result = pd.concat([df_parameter, df_matrix], axis=1)
             df_result_name = dir_2+"/df_result.csv"
-            df_result.to_csv(df_result_name, sep = ',')
+            df_result.to_csv(df_result_name, sep=',')
 
             #update the matrix
-            df_filtered = df_result.loc[df_result["target"] == df_result["target"].max()]
+            df_filtered = df_result.loc[df_result["target"]
+                                        == df_result["target"].max()]
             if len(df_filtered.index) > 1:
                 dir_warning = dir_1 + "/warning.txt"
                 f_warning = open(dir_warning, 'a')
@@ -258,17 +252,18 @@ for L in range(depth + 1):
                 f_warning.write(warning)
             matrix_result = df_filtered.values.tolist()[0][3:]
             alpha = 0
-            for x,y in order_i:
+            for x, y in order_i:
                 matrix_zero[x][y] = matrix_result[alpha]
                 alpha += 1
 
-            df_output = pd.DataFrame(matrix_zero, index=result0.index, columns=result1.index)
+            df_output = pd.DataFrame(
+                matrix_zero, index=result0.index, columns=result1.index)
             df_matrix_name = dir_2+"/df_matrix.csv"
-            df_output.to_csv(df_matrix_name, sep = ' ')
+            df_output.to_csv(df_matrix_name, sep=' ')
 
-            rounds += 1 
+            rounds += 1
 
-    order_split(num_split,num_max)
+    order_split(num_split, num_max)
 
 timeAll = datetime.datetime.now() - startTime
 print(timeAll)
